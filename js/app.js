ea326d4c70d34175b454e1a87df56d8641fe6e5f
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-03-01 22:56:25 +0600

    Js Phones add

--- js/app.js
+++ js/app.js
@@ -7,5 +7,22 @@ const searchPhone = () => {
     // console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displaysearchResult(data.data));
+}
+
+const displaysearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    for (const phone of data) {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `<div class="card" style="width: 18rem;">
+            <img src="${phone.image
+            }" class="card - img - top pt - 2" alt="...">
+            <h4 class="card-title text-center">${phone.brand}</h4>
+            <h5 class="card-subtitle text-center">${phone.phone_name}</h5>
+            <button class="btn btn-primary text-center mt-3">Datiels</button>
+    </div > `;
+        searchResult.appendChild(div);
+    };
 }
